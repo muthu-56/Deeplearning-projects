256401383457be9cd2e9c9f6cea58713d2fb4f9a
muthu-56 <marimuthu4720@gmail.com>
2020-11-15 22:07:04 +0530

    Update ANN model

--- main.py
+++ main.py
@@ -37,3 +37,35 @@ ss = StandardScaler()
 X_train = ss.fit_transform(X_train)
 X_test = ss.transform(X_test)
 
+#Start of ANN
+#Importing Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, LeakyReLU, PReLU, ELU, Dropout
+
+classifier = Sequential()
+
+#Adding first input layer and the first hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'he_uniform', activation = 'relu', input_dim = 11))
+classifier.add(Dropout(0.2))
+#Adding second hidden layer
+classifier.add(Dense(units = 6, kernel_initializer = 'he_uniform', activation = 'relu'))
+classifier.add(Dropout(0.2))
+#Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'glorot_uniform', activation = 'sigmoid'))
+
+#Compiling ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+model = classifier.fit(X_train, y_train, validation_split = 0.33, batch_size = 10, epochs = 100)
+
+
+#model.history.keys()
+
+#Predicting test data
+y_pred = classifier.predict(X_test)
+#y_pred =  (y_pred > 0.5)
+
+#Confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
